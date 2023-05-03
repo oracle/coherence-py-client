70c37e338eb251427b8d93e083a80f87debceef5
Ryan Lubke <ryan.lubke@oracle.com>
2023-05-02 21:39:37 -0700

    Add missing copyrights.

--- examples/basic/python_object_values.py
+++ examples/basic/python_object_values.py
@@ -1,4 +1,9 @@
+# Copyright (c) 2023, Oracle and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at
+# https://oss.oracle.com/licenses/upl.
+
 import asyncio
+
 from dataclasses import dataclass
 
 from coherence import NamedMap, Processors, Session
