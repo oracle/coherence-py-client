2f076d7638074dd889a9a4e6c79cecfb90ea9dbb
Dhiru Pandey <dhiru.pandey@oracle.com>
2025-04-22 16:35:13 -0700

    COH-32106 - Release Python Client 2.0.1 (#237)
    * COH-32106 - Release Python Client 2.0.1

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,5 @@
 # ----------------------------------------------------------------------------------------------------------------------
-# Copyright (c) 2022, 2024, Oracle and/or its affiliates.
+# Copyright (c) 2022, 2025, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at
 # https://oss.oracle.com/licenses/upl.
 #
@@ -23,7 +23,7 @@ print(os.path.abspath("../src"))
 sys.path.insert(0, os.path.abspath("../src"))
 
 # The full version, including alpha/beta/rc tags
-release = "2.0.0"
+release = "2.0.1"
 
 # -- Project information -----------------------------------------------------
 
--- src/coherence/__init__.py
+++ src/coherence/__init__.py
@@ -1,10 +1,10 @@
-# Copyright (c) 2022, 2024, Oracle and/or its affiliates.
+# Copyright (c) 2022, 2025, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at
 # https://oss.oracle.com/licenses/upl.
 
 from __future__ import annotations
 
-__version__ = "2.0.0"
+__version__ = "2.0.1"
 
 import contextvars
 import logging
