0757c94ef3abfa7e4945767ead452ca696b29904
Dhiru Pandey <dhiru.pandey@oracle.com>
2025-06-20 13:02:05 -0700

    Release 2.0.3 (#262)
    * Update version for release 2.0.3

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ print(os.path.abspath("../src"))
 sys.path.insert(0, os.path.abspath("../src"))
 
 # The full version, including alpha/beta/rc tags
-release = "2.0.2"
+release = "2.0.3"
 
 # -- Project information -----------------------------------------------------
 
